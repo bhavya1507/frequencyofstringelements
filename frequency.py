d2142dd344fbf1a4cae8af8102335a363d1ea4bb
bhavya1507 <69502449+bhavya1507@users.noreply.github.com>
2021-02-26 13:54:01 +0530

    Add files via upload

---
+++ frequency.py
@@ -0,0 +1,16 @@
+import operator
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    # l=list(d.items())
+    # l.sort(reverse=True)
+    # s=dict(l)
+    sorted_d=dict(sorted(d.items(), key=operator.itemgetter(1),reverse=True))
+    for i in sorted_d:
+        print(i,"=",sorted_d[i])
+str=input()
+most_frequent(str)
